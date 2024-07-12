060755f3d4c1fd4716334848c8545f08fd8fa815
mutinduhh <jonathan.mutinda@student.moringaschool.com>
2024-07-12 08:38:56 +0300

    working

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity=`roller-skate`){return `This Saturday, I want to ${activity}!`;}
+saturdayFun();
+saturdayFun(`bathe my dog`);
+const mondayWork=function(task=`go to the office`){return `This Monday, I will ${task}.`;}
+mondayWork(`work from home`);
+function wrapAdjective(symbol) {
+    return function(adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    }
+}
+let result = wrapAdjective('*');
+let emphatic = result("a hard worker");
